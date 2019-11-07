f702df3e7d1f841c39bac7d0467eec7ce78c20d2
Johan RC <johanrc.pro@gmail.com>
2019-11-07 11:15:46 +0100

    Ajout de l'authentification via JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,7 +16,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
+ * @ApiResource(
+ *     normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity("email", message="Un utilisateur ayant cet email existe dejà")
  */
 class User implements UserInterface
@@ -25,13 +27,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource"})
+     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource", "users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource"})
+     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="L'email doit être renseigné !")
      * @Assert\Email(message="L'adresse email doit avoir un format valide")
      */
@@ -51,7 +53,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource"})
+     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="le prenom est obligatoire")
      * @Assert\Length(min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères", max=255, maxMessage="Le prénom doit faire entre 3 et 255 caractères")
      */
@@ -59,7 +61,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource"})
+     * @Groups({"jojo_invoices_read", "jojo_customers_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="le nom est obligatoire")
      * @Assert\Length(min=3, minMessage="Le nom doit faire entre 3 et 255 caractères", max=255, maxMessage="Le nom doit faire entre 3 et 255 caractères")
      */
